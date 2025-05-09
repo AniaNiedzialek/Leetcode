b7564eb5f57139f3ffedfa61f4d77ea5673602fb
Ania <ania.niedzialek@sjsu.edu>
2025-05-09 14:10:05 -0700

    added question 2 - leetcode

---
+++ Solution2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Solution2 {
+    public int findNumbers(int[] nums) {
+        int size = nums.length; 
+        int even = 0;
+
+        for (int i = 0; i < size; i++) {
+            String str = Integer.toString(nums[i]);
+            int sizeString = str.length();
+            if (sizeString % 2 == 0) {
+                even++;
+            }
+        }
+        return even;       
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+
+        int[] nums = new int[size];
+        System.out.println("Enter the elements of the array separated by space:");
+        for (int i = 0; i < size; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        
+        Solution2 solution = new Solution2();
+        int result = solution.findNumbers(nums);
+        System.out.println("The number of integers with an even number of digits is: " + result);      
+        scanner.close();
+    }
+}
