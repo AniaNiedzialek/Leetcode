73d653be7eb968adf8097a45e746f8f30ef46095
Ania <ania.niedzialek@sjsu.edu>
2025-05-11 14:49:46 -0700

    added new

---
+++ DSA Review/Big O/example4.java
@@ -0,0 +1,9 @@
+
+
+public class example4 {
+    public void log(String[] names) {
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Hi" + names[i]);
+        }
+    }
+}
