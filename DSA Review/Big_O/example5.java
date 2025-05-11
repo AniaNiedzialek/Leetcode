b85ebf2189c3d1691e1ae6e568290afd17bd5b7f
Ania <ania.niedzialek@sjsu.edu>
2025-05-11 15:24:42 -0700

    new

--- DSA Review/Big_O/example5.java
+++ DSA Review/Big_O/example5.java
@@ -7,9 +7,16 @@ public class example5 {
 
         // to see the content of the array class 
         numbers[0] = 1;
-        numbers[1] = 2;
+        numbers[1] = 10;
         numbers[2] = 3;
-        System.out.println(Arrays.toString(numbers));
+
+        // another way is to do the following:
+        int[] numbers2 = {1, 2, 3};
+        
+        System.out.println("numbers1 :" + Arrays.toString(numbers));
+        System.out.println("numbers2: " + Arrays.toString(numbers2));
+
+        System.out.println(numbers.length);
     }
     
 }
