e08ec71ceabd0976680a0da47ed03633058ce374
Ania <ania.niedzialek@sjsu.edu>
2025-05-18 15:18:59 -0700

    new test cases

--- leetcode/red/MinimumSizeSubarraySum.py
+++ leetcode/red/MinimumSizeSubarraySum.py
@@ -21,3 +21,9 @@ solution = Solution()
 target = 7
 nums = [2,3,1,2,4,3]
 print(solution.minSubArrayLen(target, nums))
+target = 4
+nums = [1,4,4]
+print(solution.minSubArrayLen(target, nums))
+target = 11
+nums = [1,1,1]
+print(solution.minSubArrayLen(target, nums))
