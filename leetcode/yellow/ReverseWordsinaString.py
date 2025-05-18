60de8b6e741c6d983040717914413edbe0f78882
Ania <ania.niedzialek@sjsu.edu>
2025-05-18 11:35:27 -0700

    adding new question 151. Reverse Words in a String

---
+++ leetcode/yellow/ReverseWordsinaString.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        # print(f"{s}, {s[0]}")
+        # split the chars into separate entries
+        arr = s.split()
+        # print(arr)
+
+        reverse = list(reversed(arr))
+
+        # print(reverse)
+        return ' '.join(reverse)
+
+
+if __name__ =='__main__':
+    solution = Solution()
+    s = "the sky is blue"
+    print(f"Reversed words are: ", solution.reverseWords(s))
