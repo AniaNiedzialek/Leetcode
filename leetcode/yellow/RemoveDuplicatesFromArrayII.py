0f2786c87b92757d65887bf23c33d7f74f4a6bee
Ania <ania.niedzialek@sjsu.edu>
2025-05-22 22:31:06 -0700

    fixing a bug  and adding test case

--- leetcode/yellow/RemoveDuplicatesFromArrayII.py
+++ leetcode/yellow/RemoveDuplicatesFromArrayII.py
@@ -7,9 +7,14 @@ class Solution:
         for i in range(2, len(nums)):
             if nums[i] != nums[count - 2]:
                 nums[count] = nums[i]
-                count += 2
+                count += 1
         print(nums)
         return count
+    
+    
 solution = Solution()
-nums = [1,1,1,2,2,3,]
+nums = [1,1,1,2,2,3]
+print(solution.removeDuplicates(nums))
+solution = Solution()
+nums = [0,0,1,1,1,1,2,3,3]
 print(solution.removeDuplicates(nums))
