e9e0a3aa7d45e0e252ee80622e50b18314916d37
Ania <ania.niedzialek@sjsu.edu>
2025-05-23 13:18:42 -0700

    adding recursive info

---
+++ DSA Review/refresher/recursion.py
@@ -0,0 +1,14 @@
+# Fibonacci
+# F(0) = 0
+# F(1) = 1
+# n > 1: F(n) = F(n-1) + F(N-2)
+
+def F(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return F(n - 1) + F(n - 2)
+
+print(F(5))
