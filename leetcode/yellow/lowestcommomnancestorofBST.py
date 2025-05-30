b941d772b3a0fa5f90ead7f608a6fd0f8294e0b4
Ania <ania.niedzialek@sjsu.edu>
2025-05-30 15:10:43 -0700

    adding test cases

--- leetcode/yellow/lowestcommomnancestorofBST.py
+++ leetcode/yellow/lowestcommomnancestorofBST.py
@@ -19,3 +19,19 @@ class Solution:
                 cur = cur.right
             else:
                 return cur
+            
+if __name__ == "__main__":
+    root = TreeNode(6)
+    root.left = TreeNode(2)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode (0)
+    root.left.right = TreeNode(4)
+    root.left.right.left = TreeNode(3)
+    root.left.right.right = TreeNode(5)
+    root.right.left = TreeNode(7)
+    root.right.right = TreeNode(9)
+    
+    p = root.left
+    q = root.right
+    solution = Solution()
+    print(solution.lowestCommonAncestor(root, p, q).val)  # Output: 6
