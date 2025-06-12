eb892e34bc96b883fdbf684bc7954fb88088385c
Ania <ania.niedzialek@sjsu.edu>
2025-06-12 19:52:08 +0200

    adding jump game II

---
+++ leetcode/yellow/jumpGameII.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    
+    def jump(self, nums: List[int]) -> int:
+        smallest = 0
+        end, far = 0, 0
+
+        for i in range(len(nums) - 1):
+            far = max(far, i + nums[i])
+
+            if i == end:
+                smallest += 1
+                end = far
+
+        return smallest
