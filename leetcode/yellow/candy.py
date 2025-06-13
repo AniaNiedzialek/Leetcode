dffbbb4838632523310c499ccce60cc55ab540d7
Ania <ania.niedzialek@sjsu.edu>
2025-06-13 20:28:38 +0200

    adding test cases

--- leetcode/yellow/candy.py
+++ leetcode/yellow/candy.py
@@ -14,3 +14,7 @@ class Solution:
             
         return sum(arr)
 
+solution = Solution()
+print(solution.candy([1,0,2]))  # Expected: 5
+print(solution.candy([1,2,2]))  # Expected: 4
+print(solution.candy([1,3,2,2,1]))  # Expected: 9   
