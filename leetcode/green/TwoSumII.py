c631fb3bdb93900171884f4cf30d858b565c312b
Ania <ania.niedzialek@sjsu.edu>
2025-06-15 16:33:29 +0200

    adding more test cases

--- leetcode/green/TwoSumII.py
+++ leetcode/green/TwoSumII.py
@@ -16,3 +16,5 @@ class Solution:
 
 solution = Solution()
 print(solution.twoSum([2, 7, 11, 15], 9))  # Output: [1, 2]
+print(solution.twoSum([2, 3, 4], 6))  # Output: [1, 3]  
+print(solution.twoSum([-1, 0], -1))  # Output: [1, 2]
