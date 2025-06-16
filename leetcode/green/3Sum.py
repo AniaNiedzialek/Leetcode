54f16147941773412de5954a698831d47ab3a35b
Ania <ania.niedzialek@sjsu.edu>
2025-06-16 13:34:26 +0200

    adding a new question - 15.3sum - medium

---
+++ leetcode/green/3Sum.py
@@ -0,0 +1,41 @@
+
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        answer = []
+
+
+        for i in range(n):
+            # base case 1 -  if nums[i] > 0:
+            if nums[i] > 0:
+                break
+            # base case 2 - if i is bigger than zero but the next entry is the same
+            elif i > 0 and nums[i] == nums[i - 1]:
+                continue
+            # lo and hi
+            lo, hi =  i + 1, n - 1
+            while lo < hi:
+                # get the sum
+                summ = nums[i] + nums[lo] + nums[hi]
+                # case 1 - we found the result
+                if summ == 0:
+                    answer.append([nums[i], nums[lo], nums[hi]])
+                    lo, hi = lo + 1, hi - 1
+                    # case 1 - if the next number for lo is the same as current
+                    while lo < hi and nums[lo] == nums[lo - 1]:
+                        lo += 1
+                    while lo < hi and nums[hi] == nums[hi + 1]:
+                        hi -= 1
+                elif summ < 0:
+                    lo += 1
+                else:
+                    hi -= 1
+            
+
+
+
+
+
+        return answer
