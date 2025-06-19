f278c33b40a3d5a880ea99b82717eb70389a0d90
Ania <ania.niedzialek@sjsu.edu>
2025-06-19 16:11:56 +0200

    Adding test cases

--- leetcode/green/minimumNumberOfArrowsToBurstBalloons.py
+++ leetcode/green/minimumNumberOfArrowsToBurstBalloons.py
@@ -14,15 +14,20 @@ class Solution:
 
             # case 1 - if overlapping
             print(curr[0])
-            print(f"prev is {prev[1]}")
+            # print(f"prev is {prev[1]}")
             if curr[0] <= prev[1]:
-                print(f"hello")
+                # print(f"hello")
                 # adjust result - number of arrows
                 res -= 1
                 # adjust prev
                 prev = [curr[0], min(curr[1], prev[1])]
-                print(f"new prev is {prev}")
+                # print(f"new prev is {prev}")
             # case 2 - not overlapping
             else:
                 prev = curr
         return res
+    
+    
+solution = Solution()
+print(solution.findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]))  # Output: 2
+print(solution.findMinArrowShots([[1,2],[3,4],[5,6],[7,8]]))  # Output:
