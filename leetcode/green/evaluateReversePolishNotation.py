8b7b92c7175c353918211ba5637f093e5722c659
Ania <ania.niedzialek@sjsu.edu>
2025-06-20 17:21:57 +0200

    adding a new question

---
+++ leetcode/green/evaluateReversePolishNotation.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        
+        for c in tokens:
+            # step 1 - write 4 cases
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                first, second = stack.pop(), stack.pop()
+                stack.append(second - first)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                first, second = stack.pop(), stack.pop()
+                stack.append(int(second / first))
+
+            else:
+                stack.append(int(c))
+        return stack.pop()
