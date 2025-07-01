d257dc9a6f94aebdb9433074622f28df41f88487
Ania <ania.niedzialek@sjsu.edu>
2025-07-01 15:15:31 +0200

    Adding test cases

--- leetcode/green/removeNthNodeFromTheEndOfList.py
+++ leetcode/green/removeNthNodeFromTheEndOfList.py
@@ -27,3 +27,36 @@ class Solution:
         left.next = left.next.next
         
         return dummy.next
+    
+    # test cases
+  # ...existing code...
+
+def print_linked_list(head):
+    vals = []
+    while head:
+        vals.append(head.val)
+        head = head.next
+    print(vals)
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    # Test case 1: Remove 2nd node from end from [1,2,3,4,5]
+    head1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    result1 = solution.removeNthFromEnd(head1, 2)
+    print_linked_list(result1)  # Expected: [1, 2, 3, 5]
+
+    # Test case 2: Remove 1st node from end from [1]
+    head2 = ListNode(1)
+    result2 = solution.removeNthFromEnd(head2, 1)
+    print_linked_list(result2)  # Expected: []
+
+    # Test case 3: Remove 1st node from end from [1,2]
+    head3 = ListNode(1, ListNode(2))
+    result3 = solution.removeNthFromEnd(head3, 1)
+    print_linked_list(result3)  # Expected: [1]
+
+    # Test case 4: Remove 2nd node from end from [1,2]
+    head4 = ListNode(1, ListNode(2))
+    result4 = solution.removeNthFromEnd(head4, 2)
+    print_linked_list(result4)  # Expected: [2]
