a6eb125adbc9fe128b0a2594e4dc346046a675fc
Ania <ania.niedzialek@sjsu.edu>
2025-07-03 18:21:25 +0200

    adding test cases

--- leetcode/green/factorial.py
+++ leetcode/green/factorial.py
@@ -8,3 +8,10 @@ class Solution:
             limit *= 5
         return count
     
+    
+# test cases
+solution = Solution()
+print(solution.trailingZeroes(3))  # Expected: 0
+print(solution.trailingZeroes(5))  # Expected: 1
+print(solution.trailingZeroes(10))  # Expected: 2
+print(solution.trailingZeroes(25))  # Expected: 6   
