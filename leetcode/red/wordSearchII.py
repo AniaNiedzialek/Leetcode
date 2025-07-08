96414230dda3e6044ec827cfd54eade7c82882b3
Ania <ania.niedzialek@sjsu.edu>
2025-07-08 22:04:57 +0200

    adding a new question

---
+++ leetcode/red/wordSearchII.py
@@ -0,0 +1,51 @@
+from typing import List
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        class TrieNode:
+            def __init__(self):
+                self.children = {}
+                self.word = None
+
+        def build_trie(words):
+            root = TrieNode()
+            for word in words:
+                node = root
+                for c in word:
+                    if c not in node.children:
+                        node.children[c] = TrieNode()
+                    node = node.children[c]
+                node.word = word
+            return root
+
+
+        # board
+        root = build_trie(words)
+        row, col = len(board), len(board[0])
+        result = []
+
+        def dfs(r, c, node):
+            char = board[r][c]
+            if char not in node.children:
+                return
+
+            next_node = node.children[char]
+            if next_node.word:
+                result.append(next_node.word)
+                next_node.word = None  # prevent duplicates
+
+            board[r][c] = "#"  # mark visited
+
+            for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < row and 0 <= nc < col and board[nr][nc] != "#":
+                    dfs(nr, nc, next_node)
+
+            board[r][c] = char  # backtrack
+
+        for r in range(row):
+            for c in range(col):
+                dfs(r, c, root)
+
+        return result
