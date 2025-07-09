d57aaceea563d41094166814da98ba263492e110
Ania <ania.niedzialek@sjsu.edu>
2025-07-09 17:34:13 +0200

    adding new question

---
+++ leetcode/yellow/permutations.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[str]) -> List[str]:
+        
+        # result
+        res = []
+        
+        # function backtracking
+        def back(path, used):
+            # base case
+            if len(path) == len(nums):
+                res.append(path.copy())
+                return
+           
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                used[i] = True
+                path.append(nums[i])
+                back(path, used)
+                path.pop()
+                
+                used[i] = False
+            
+            
+        back([], [False] * len(nums))
+        return res
