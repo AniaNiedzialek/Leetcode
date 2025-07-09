9999caad53e9a720bb33c332c228e7b4915eeffa
Ania <ania.niedzialek@sjsu.edu>
2025-07-09 20:15:20 +0200

    Adding new

--- leetcode/yellow/numberOf1Bits.py
+++ leetcode/yellow/numberOf1Bits.py
@@ -5,3 +5,19 @@ class Solution:
             res += n % 2
             n = n  >> 1
         return res
+    
+    # ...existing code...
+
+if __name__ == "__main__":
+    solution = Solution()
+    # Test case 1: 11 (binary 1011) has 3 ones
+    print(solution.hammingWeight(11))  # Expected: 3
+
+    # Test case 2: 128 (binary 10000000) has 1 one
+    print(solution.hammingWeight(128))  # Expected: 1
+
+    # Test case 3: 0 has 0 ones
+    print(solution.hammingWeight(0))  # Expected: 0
+
+    # Test case 4: 4294967295 (all 32 bits set)
+    print(solution.hammingWeight(0xFFFFFFFF))  # Expected: 32
