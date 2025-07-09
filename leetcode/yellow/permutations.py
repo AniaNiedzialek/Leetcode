3073f84841ea074c34fca7c8e0d2235c6d43ed4d
Ania <ania.niedzialek@sjsu.edu>
2025-07-09 17:37:00 +0200

    adding a new question

--- leetcode/yellow/permutations.py
+++ leetcode/yellow/permutations.py
@@ -32,4 +32,10 @@ sol = Solution()
 nums = [1,2,3]
 print("Test Case 1: ", sol.permute(nums))
     
-    
+# 2
+nums2 = [1]
+print("Test Case 2: ", sol.permute(nums2)) # Expected: [[1]]
+
+# 3 
+nums3 = []
+print("Test Case 3: ", sol.permute(nums3)) # Expect: [[]]
