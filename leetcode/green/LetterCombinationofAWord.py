a2a397f9443324eab35d356778c890a9a4cdbdd5
Ania <ania.niedzialek@sjsu.edu>
2025-07-09 14:59:49 +0200

    adding a new question

---
+++ leetcode/green/LetterCombinationofAWord.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution():
+    def letterCombinations(self, digits:str) -> List[str]:
+        # declare the result array to return
+        res = []
+        # create the mapping
+        digitMap = {
+             "2" : "abc",
+            "3" :  "def",
+            "4" : "ghi",
+            "5" : "jkl", 
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
+        }
+        
+        # define the backtracking function
+        def back(i, curStr):
+            # base case - if passed
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            # otherwise keep checking recursively
+            for c in digitMap[digits[i]]:
+                back(i + 1, curStr + c)
+                
+        # call the function
+        if digits:
+            back(0, "")
+            
+        return res
+        
