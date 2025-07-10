5b34958d7f179826a4fdfc5c07bf8efce3a9a204
Ania <ania.niedzialek@sjsu.edu>
2025-07-10 23:51:16 +0200

    test cases

--- leetcode/yellow/FirstAndLastPositionOfSortedArray.py
+++ leetcode/yellow/FirstAndLastPositionOfSortedArray.py
@@ -26,3 +26,38 @@ class Solution:
                     l = m + 1
 
         return i
+
+# ...existing code...
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    # Test case 1: Target appears multiple times
+    nums1 = [5,7,7,8,8,10]
+    target1 = 8
+    print(solution.searchRange(nums1, target1))  # Expected: [3, 4]
+
+    # Test case 2: Target not present
+    nums2 = [5,7,7,8,8,10]
+    target2 = 6
+    print(solution.searchRange(nums2, target2))  # Expected: [-1, -1]
+
+    # Test case 3: Target is the only element
+    nums3 = [2,2,2,2,2]
+    target3 = 2
+    print(solution.searchRange(nums3, target3))  # Expected: [0, 4]
+
+    # Test case 4: Empty array
+    nums4 = []
+    target4 = 0
+    print(solution.searchRange(nums4, target4))  # Expected: [-1, -1]
+
+    # Test case 5: Target at the beginning
+    nums5 = [1,2,3,4,5]
+    target5 = 1
+    print(solution.searchRange(nums5, target5))  # Expected: [0, 0]
+
+    # Test case 6: Target at the end
+    nums6 = [1,2,3,4,5]
+    target6 = 5
+    print(solution.searchRange(nums6, target6))  # Expected: [4, 4]
