94247115a1c677d01eab129dfde5bff541d35ff4
Ryszard Niedzialek <ania.niedzialek@sjsu.edu>
2025-07-17 13:08:28 +0200

    adding test cases

--- leetcode/green/MaximumSubarray.py
+++ leetcode/green/MaximumSubarray.py
@@ -10,3 +10,18 @@ class Solution:
             max_sum = max(max_sum, cur_sum)
 
         return max_sum
+    
+    
+    
+# test cases
+solution = Solution()
+num1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(f"Test Case 1: ", solution.maxSubarry(num1))  # expected: 6
+
+ # 2
+num2 = [0]
+print(f"Test Case 2: ", solution.maxSubarry(num2)) #expected: 0
+
+# 3
+num3 = [1, -2, -4, -3, -9, 0, 1] 
+print(f"Test Case 3: ", solution.maxSubarry(num3)) # expected 1
