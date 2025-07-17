c00e2ac64bead1fbdb7d68af5045b6c52f4ddc05
AN <ania.niedzialek@sjsu.edu>
2025-07-17 14:34:33 +0000

    adding test cases and updates

--- leetcode/green/medianOfTwoSortedArrays_BRUTE.py
+++ leetcode/green/medianOfTwoSortedArrays_BRUTE.py
@@ -8,24 +8,25 @@ class Solution:
         combined += nums1
         combined += nums2
         combined.sort()
-        print(combined)
         l, r = 0, len(combined) - 1
-        print(l, r)
 
         mid = (l + r) // 2
-        print(f"mid is {mid}, l is {l}, r is {r}, {combined[l]}, {combined[r]}")
-        print(mid)
 
         for i in combined:
             if len(combined) % 2 == 1:
-                print(f"hello")
+           
                 res = combined[mid]
 
             else:
-                print(f"{mid} is {combined[mid]}")
                 res = (combined[mid] + combined[mid + 1]) / 2
 
         
             
 
         return res
+    
+# test case
+solutuion = Solution()
+nums1 = [1, 3]
+nusm2 = [2]
+print(f"Test Case 1: ", solutuion.findMedianSortedArrays(nums1, nusm2))  # expected: 2.0
